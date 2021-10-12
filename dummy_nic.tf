81223730703f1915420c2823e64a90861d834f9b
U-DLTAEHS\Teo <Daniel>
2021-10-12 13:30:48 +0800

    Variable correction

--- dummy_nic.tf
+++ dummy_nic.tf
@@ -1,19 +1,19 @@
 resource "azurerm_network_interface" "dummy-nic-ip-reservation" {
-  name                = var.nic_name
-  location            = var.location
-  resource_group_name = var.rg_name
+  name                = "${var.nic_name}"
+  location            = "${var.location}"
+  resource_group_name = "${var.rg_name}"
 
   ip_configuration {
     name                          = format("private-", var.nic_name)
-    subnet_id                     = var.subnet_id
-    private_ip_address_allocation = var.private_ip_address_allocation
-    private_ip_address            = var.private_ip_address
+    subnet_id                     = "${var.subnet_id}"
+    private_ip_address_allocation = "${var.private_ip_address_allocation}"
+    private_ip_address            = "${var.private_ip_address}"
   }
 
   tags = {
-    BusinessApplication = var.biz_application
-    ApplicationService = var.application_service
-    CostCenter = var.costcester
-    CreatedOnDate = var.creationdate
+    BusinessApplication = "${var.biz_application}"
+    ApplicationService = "${var.application_service}"
+    CostCenter = "${var.costcester}"
+    CreatedOnDate = "${var.creationdate}"
   }
 }
