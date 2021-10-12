e2d8ae6405ffb34606e54dfef823fd3d8e155a36
U-DLTAEHS\Teo <Daniel>
2021-10-12 15:53:59 +0800

    changes of file name

--- dummy_nic.tf
+++ dummy_nic.tf
@@ -1,19 +1,25 @@
 resource "azurerm_network_interface" "dummy-nic-ip-reservation" {
-  name                = var.nic_name
-  location            = var.location
-  resource_group_name = var.rg_name
+  name                = "${var.nic_name}"
+  location            = "${var.location}"
+  resource_group_name = "${var.rg_name}"
 
   ip_configuration {
-    name                          = format("private-", var.nic_name)
-    subnet_id                     = var.subnet_id
-    private_ip_address_allocation = var.private_ip_address_allocation
-    private_ip_address            = var.private_ip_address
+    name                          = "private-.${var.nic_name}"
+    subnet_id                     = "${data.azurerm_subnet.example.id}"
+    private_ip_address_allocation = "${var.private_ip_address_allocation}"
+    private_ip_address            = "${var.private_ip_address}"
   }
 
   tags = {
-    BusinessApplication = var.biz_application
-    ApplicationService = var.application_service
-    CostCenter = var.costcester
-    CreatedOnDate = var.creationdate
+    BusinessApplication = "${var.biz_application}"
+    ApplicationService = "${var.application_service}"
+    CostCenter = "${var.costcester}"
+    CreatedOnDate = "${var.creationdate}"
   }
+}
+
+data "azurerm_subnet" "example" {
+  name                 = "subnet-A"
+  virtual_network_name = "common-vnet"
+  resource_group_name  = "${var.rg_name}"
 }
---
+++ vars.tf
@@ -0,0 +1,26 @@
+variable "nic_name" {
+}
+
+variable "location" {
+}
+
+variable "rg_name" {
+}
+
+variable "private_ip_address_allocation" {
+}
+
+variable "private_ip_address" {
+}
+
+variable "biz_application" {
+}
+
+variable "application_service" {
+}
+
+variable "costcester" {
+}
+
+variable "creationdate" {
+}
