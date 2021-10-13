1472eb85a134a0254af68b18f9978082f7511b44
U-DLTAEHS\Teo <Daniel>
2021-10-13 09:36:55 +0800

    change file name and variables

---
+++ create_nic.tf
@@ -0,0 +1,25 @@
+resource "azurerm_network_interface" "dummy-nic-ip-reservation" {
+  name                = "${var.nic_name}"
+  location            = "${var.location}"
+  resource_group_name = "${var.rg_name}"
+
+  ip_configuration {
+    name                          = "private-.${var.nic_name}"
+    subnet_id                     = "${data.azurerm_subnet.subnetdata.id}"
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "${var.private_ip_address}"
+  }
+
+  tags = {
+    BusinessApplication = "${var.biz_application}"
+    ApplicationService = "${var.application_service}"
+    CostCenter = "${var.costcester}"
+    CreatedOnDate = "${var.creationdate}"
+  }
+}
+
+data "azurerm_subnet" "subnetdata" {
+  name                 = "${var.subnet_name}"
+  virtual_network_name = "${var.vnet_name}"
+  resource_group_name  = "${var.rg_name}"
+}
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "nic_name" {}
+variable "location" {}
+variable "rg_name" {}
+variable "private_ip_address_allocation" {}
+variable "private_ip_address" {}
+variable "biz_application" {}
+variable "application_service" {}
+variable "costcester" {}
+variable "creationdate" {}
+variables "subnet_name" {}
+variables "vnet_name" {}
+variables "rg_name" {}
